a07285afaeb37c6214842c14a44c8748c6b1a8af
Hyeon Jun Kim <hjk013@gmail.com>
2019-04-15 13:56:29 -0700

    created search params

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import { render } from "react-dom";
 import { Router, Link } from "@reach/router";
 import Results from "./Results";
 import Detail from "./Details";
+import SearchParams from "./SearchParams";
 
 const App = () => (
   <div>
@@ -12,6 +13,7 @@ const App = () => (
     <Router>
       <Results path="/" />
       <Detail path="/details/:id" />
+      <SearchParams path="/search-params" />
     </Router>
   </div>
 );
